cae0f4054e797c769a7c775214d9427b3cb35064
sheep2652014 <sheep3652003@tom.com>
2015-09-23 16:44:00 +0800

    first
    Change-Id: I9e34257b2cde7dd90fa03fe8d64f164814e6f6e5

---
+++ obj/local/armeabi/objs/com_apress_threads_MainActivity/com_apress_threads_MainActivity.o.d
@@ -0,0 +1,137 @@
+obj/local/armeabi/objs/com_apress_threads_MainActivity/com_apress_threads_MainActivity.o: \
+ jni/com_apress_threads_MainActivity.cpp \
+ D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/stdio.h \
+ D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/sys/cdefs.h \
+ D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h \
+ D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/android/api-level.h \
+ D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/sys/types.h \
+ D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/stdint.h \
+ D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/sys/_types.h \
+ D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/machine/_types.h \
+ D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/linux/posix_types.h \
+ D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/linux/stddef.h \
+ D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/linux/compiler.h \
+ D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/asm/posix_types.h \
+ D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/asm/types.h \
+ D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/linux/types.h \
+ D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/machine/kernel.h \
+ D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/sys/sysmacros.h \
+ D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/unistd.h \
+ D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/sys/select.h \
+ D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/sys/time.h \
+ D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/linux/time.h \
+ D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/signal.h \
+ D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/limits.h \
+ D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/sys/limits.h \
+ D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/linux/limits.h \
+ D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/machine/internal_types.h \
+ D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/machine/limits.h \
+ D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/sys/syslimits.h \
+ D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/asm/page.h \
+ D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/string.h \
+ D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/malloc.h \
+ D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/asm/signal.h \
+ D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/asm-generic/signal.h \
+ D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/asm/sigcontext.h \
+ D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/asm/siginfo.h \
+ D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/asm-generic/siginfo.h \
+ D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/sys/ucontext.h \
+ D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/sys/user.h \
+ D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/sys/sysconf.h \
+ D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/linux/capability.h \
+ D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/pathconf.h \
+ D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/pthread.h \
+ D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/time.h \
+ D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/sched.h \
+ jni/com_apress_threads_MainActivity.h \
+ D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/jni.h
+
+D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/stdio.h:
+
+D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/sys/cdefs.h:
+
+D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h:
+
+D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/android/api-level.h:
+
+D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/sys/types.h:
+
+D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/stdint.h:
+
+D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/sys/_types.h:
+
+D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/machine/_types.h:
+
+D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/linux/posix_types.h:
+
+D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/linux/stddef.h:
+
+D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/linux/compiler.h:
+
+D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/asm/posix_types.h:
+
+D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/asm/types.h:
+
+D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/linux/types.h:
+
+D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/machine/kernel.h:
+
+D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/sys/sysmacros.h:
+
+D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/unistd.h:
+
+D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/sys/select.h:
+
+D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/sys/time.h:
+
+D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/linux/time.h:
+
+D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/signal.h:
+
+D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/limits.h:
+
+D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/sys/limits.h:
+
+D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/linux/limits.h:
+
+D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/machine/internal_types.h:
+
+D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/machine/limits.h:
+
+D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/sys/syslimits.h:
+
+D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/asm/page.h:
+
+D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/string.h:
+
+D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/malloc.h:
+
+D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/asm/signal.h:
+
+D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/asm-generic/signal.h:
+
+D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/asm/sigcontext.h:
+
+D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/asm/siginfo.h:
+
+D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/asm-generic/siginfo.h:
+
+D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/sys/ucontext.h:
+
+D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/sys/user.h:
+
+D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/sys/sysconf.h:
+
+D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/linux/capability.h:
+
+D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/pathconf.h:
+
+D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/pthread.h:
+
+D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/time.h:
+
+D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/sched.h:
+
+jni/com_apress_threads_MainActivity.h:
+
+D:/androidndkr10e/platforms/android-19/arch-arm/usr/include/jni.h:
